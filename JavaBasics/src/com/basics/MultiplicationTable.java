9a8b75d769df2814f0999c6271d9e9e9bac24f5a
sathyapriya-t <sathya2001t@gmail.com>
2024-03-07 01:07:53 +0530

    First commit

---
+++ JavaBasics/src/com/basics/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package com.basics;
+
+public class MultiplicationTable {
+	
+	public void generateTable(int table, int times) {
+		for(int i =1;i<=times;i++) {
+			System.out.printf("%d * %d = %d", i,table,i*table).println();
+			
+		}
+	}
+	public static void main(String[] args) {
+		MultiplicationTable five = new MultiplicationTable();
+		five.generateTable(5, 10);
+		MultiplicationTable ten = new MultiplicationTable();
+		ten.generateTable(10, 100);
+
+
+	}
+
+}
