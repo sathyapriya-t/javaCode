6da408d05f6af93373d76fb0dfdb63fffdf47d43
sathya.Priya <sathya.Priya@sainsburys.co.uk>
2024-03-09 09:57:00 +0530

    char and alphabets

---
+++ JavaBasics/src/com/oops/character/CharRunner.java
@@ -0,0 +1,22 @@
+package com.oops.character;
+
+public class CharRunner {
+
+	public static void main(String[] args) {
+		MyChar digit = new MyChar('1');
+		MyChar vowel = new MyChar('A');
+		MyChar alphabet = new MyChar('a');
+		
+	
+		System.out.println(digit.isDigit());
+		System.out.println(vowel.isVowel());
+		System.out.println(alphabet.isAlphabet());
+		System.out.println(digit.isUpperCase());
+		System.out.println(digit.isLowerCase());
+		
+		MyChar.printUpperCaseAlphabets();
+		MyChar.printLowerCaseAlphabets();
+
+	}
+
+}
